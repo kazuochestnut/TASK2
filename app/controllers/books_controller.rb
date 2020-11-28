9d7657337a4dd60cd706fa7f235f9236ddb8c520
kazuochestnut <kazuochestnut@gmail.com>
2020-11-28 09:30:59 +0000

    first commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,60 @@
+class BooksController < ApplicationController
+  
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def new
+  end
+  
+  def show
+    @book = Book.find(params[:id])
+  end
+
+
+  def create
+    @book = Book.new(book_params)
+     
+     if @book.save
+      redirect_to book_path(@book.id)
+      flash[:complete]="Book was successfully created."
+     elsif
+      @books = Book.all
+      render :index
+      flash[:complete]=""
+     else
+      @books = Book.all
+      flash[:complete]="Failed to save"
+      render :index
+     end
+     
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book)
+      flash[:complete]="Book was successfully updated."
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    flash[:complete]="Book was successfully destroyed."
+    redirect_to books_path
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title ,:body)
+  end
+
+end
